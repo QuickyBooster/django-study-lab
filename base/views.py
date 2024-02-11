6c31efc34df7acef2371f2b40daf6591cca3d4d4
QuickyBooster <quicklybooster.vh@gmail.com>
2024-02-12 00:01:56 +0700

    add recent activities

--- base/views.py
+++ base/views.py
@@ -66,24 +66,26 @@ def home(request):
 
     room_count = rooms.count()
     topics = Topic.objects.all()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
     roomMessages = room.message_set.all().order_by('-created')
-
+    participants = room.participants.all()
     if request.method == 'POST':
         message = Message.objects.create(
             user=request.user,
             room=room,
             body=request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
 
-    context = {'room': room, 'roomMessages': roomMessages}
+    context = {'room': room, 'roomMessages': roomMessages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 
@@ -127,3 +129,14 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
+
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+    if request.user != message.user:
+        return HttpResponse("You're not allowed here")
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': message})
